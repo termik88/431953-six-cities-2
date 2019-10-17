a59c5745e55bf64231b34519dbe3e839c1a4657a
termik88 <dirkser@mail.ru>
2019-10-17 17:01:03 +0500

    начало работы

---
+++ src/component/app/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Main from '../main/main.jsx';
+
+const App = () => {
+
+  return <Main />;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './component/app/app.jsx';
+
+const init = () => {
+  ReactDOM.render(
+      <App/>,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
